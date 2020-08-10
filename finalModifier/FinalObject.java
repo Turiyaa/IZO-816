75b9711368b7a81589a6ee5f88adda105c7ba0ad
Turiyaa <nneopane@oswego.edu>
2020-08-10 11:39:17 -0400

    Final modifier on Object

---
+++ finalModifier/FinalObject.java
@@ -0,0 +1,12 @@
+//Object reference is constant, but the data is the class are not
+
+public class FinalObject {
+
+  public static void main(String [] args){
+
+    final StringBuilder cobra = new StringBuilder();
+    cobra.append("20 minutes of life");
+    cobra.append("19 minutes of life");
+
+  }
+}
